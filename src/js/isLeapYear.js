51c736dd1452a30d70bf5ae973af54528e77139b
VPresich <valya.alexandrova.presich@gmail.com>
2024-05-17 21:32:10 +0200

    added unit tests

--- src/js/isLeapYear.js
+++ src/js/isLeapYear.js
@@ -1,7 +1,23 @@
 const isLeapYear = (year) => {
-  const res = true;
+  if (year === undefined) {
+    throw new Error("Year must be exist");
+  }
 
-  return res;
+  if (typeof year !== "number") {
+    throw new Error("Year must be a number");
+  }
+
+  if (!Number.isInteger(year)) {
+    throw new Error("Year must be integer");
+  }
+
+  if (year < 42) {
+    throw new Error("Year must be > 42");
+  }
+
+  const date = new Date(year, 2, 0);
+  const febraryDays = date.getDate();
+  return febraryDays === 29;
 };
 
 export default isLeapYear;
--- src/tests/isLeapYear.test.js
+++ src/tests/isLeapYear.test.js
@@ -2,12 +2,58 @@ import isLeapYear from "../js/isLeapYear.js";
 
 describe("Test of isLeapYear function", () => {
   test("2008 - true", () => {
-    const result = isLeapYear(2008);
-    expect(result).toBe(true);
+    expect(isLeapYear(2008)).toBe(true);
   });
 
-  test("2000 - false", () => {
-    const result = isLeapYear(2000);
-    expect(result).toBe(true);
+  test("2000 - true", () => {
+    expect(isLeapYear(2000)).toBe(true);
+  });
+
+  test("2003 - false", () => {
+    expect(isLeapYear(2003)).toBe(false);
+  });
+
+  test("1900 - false", () => {
+    expect(isLeapYear(1900)).toBe(false);
+  });
+
+  test('41 - error "Year must be > 42"', () => {
+    expect(() => isLeapYear(40)).toThrow("Year must be > 42");
+  });
+
+  test('2008.5 - error "Year must be integer"', () => {
+    expect(() => isLeapYear(2008.5)).toThrow("Year must be integer");
+  });
+
+  test('() - error "Year must be integer"', () => {
+    expect(() => isLeapYear()).toThrow("Year must be exist");
+  });
+
+  test('"2008" - error "Year must be a number"', () => {
+    expect(() => isLeapYear("2008")).toThrow("Year must be a number");
+  });
+
+  test('null - error "Year must be a number"', () => {
+    expect(() => isLeapYear(null)).toThrow("Year must be a number");
+  });
+
+  test('true - error "Year must be a number"', () => {
+    expect(() => isLeapYear(true)).toThrow("Year must be a number");
+  });
+
+  test('false - error "Year must be a number"', () => {
+    expect(() => isLeapYear(false)).toThrow("Year must be a number");
+  });
+
+  test('()=>{} - error "Year must be a number"', () => {
+    expect(() => isLeapYear(() => {})).toThrow("Year must be a number");
+  });
+
+  test('{} - error "Year must be a number"', () => {
+    expect(() => isLeapYear({})).toThrow("Year must be a number");
+  });
+
+  test('[] - error "Year must be a number"', () => {
+    expect(() => isLeapYear([])).toThrow("Year must be a number");
   });
 });
